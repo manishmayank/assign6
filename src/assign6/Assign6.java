35c9202bfcb16e4d2c2153991110d59054601de1
manishmayank <manishmayank93@gmail.com>
2014-03-31 22:19:48 +0530

    yo yo

---
+++ src/assign6/Assign6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assign6;
+
+/**
+ *
+ * @author mayank manish
+ */
+public class Assign6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
