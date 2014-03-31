63e1b624b6848d168f1afe6e8fcbfdeddf2f93bf
manishmayank <manishmayank93@gmail.com>
2014-03-31 22:32:07 +0530

    new

--- src/assign6/Assign6.java
+++ src/assign6/Assign6.java
@@ -12,7 +12,10 @@ package assign6;
  */
 public class Assign6 {
      public int a;
-     sazdscgbnm
+     sazdscgbnmzcxcbv ;
+     
+     
+             
     /**
      * @param args the command line arguments
      */
